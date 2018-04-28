51df2603ab6ebb6870c1ecb32ba852dc2fe91434
Daniel Czerwonk <daniel@dan-nrw.de>
2018-04-28 14:53:56 +0200

    added metrics for optical monitoring

--- collector/optics_collector.go
+++ collector/optics_collector.go
@@ -1,6 +1,13 @@
 package collector
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"strconv"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/routeros.v2/proto"
+)
 
 type opticsCollector struct {
 	rxStatusDesc    *prometheus.Desc
@@ -10,6 +17,7 @@ type opticsCollector struct {
 	temperatureDesc *prometheus.Desc
 	txBiasDesc      *prometheus.Desc
 	voltageDesc     *prometheus.Desc
+	props           []string
 }
 
 func newOpticsCollector() routerOSCollector {
@@ -24,6 +32,7 @@ func newOpticsCollector() routerOSCollector {
 		temperatureDesc: description(prefix, "temperature_celsius", "temperature in degree celsius", labelNames),
 		txBiasDesc:      description(prefix, "tx_bias_ma", "bias is milliamps", labelNames),
 		voltageDesc:     description(prefix, "voltage_volt", "volage in volt", labelNames),
+		props:           []string{"sfp-rx-loss", "sfp-tx-fault", "sfp-temperature", "sfp-supply-voltage", "sfp-tx-bias-current", "sfp-tx-power", "sfp-rx-power"},
 	}
 }
 
@@ -38,5 +47,107 @@ func (c *opticsCollector) describe(ch chan<- *prometheus.Desc) {
 }
 
 func (c *opticsCollector) collect(ctx *collectorContext) error {
+	reply, err := ctx.client.Run("/interface/ethernet/print", "=.proplist=name")
+	if err != nil {
+		log.WithFields(log.Fields{
+			"device": ctx.device.Name,
+			"error":  err,
+		}).Error("error fetching interface metrics")
+		return err
+	}
+
+	ifaces := make([]string, 0)
+	for _, iface := range reply.Re {
+		n := iface.Map["name"]
+		if strings.HasPrefix(n, "sfp") {
+			ifaces = append(ifaces, n)
+		}
+	}
+
+	if len(ifaces) == 0 {
+		return nil
+	}
+
+	return c.collectOpticalMetricsForInterfaces(ifaces, ctx)
+}
+
+func (c *opticsCollector) collectOpticalMetricsForInterfaces(ifaces []string, ctx *collectorContext) error {
+	reply, err := ctx.client.Run("/interface/ethernet/monitor",
+		"=numbers="+strings.Join(ifaces, ","),
+		"=once=",
+		"=.proplist=name,"+strings.Join(c.props, ","))
+	if err != nil {
+		log.WithFields(log.Fields{
+			"device": ctx.device.Name,
+			"error":  err,
+		}).Error("error fetching interface monitor metrics")
+		return err
+	}
+
+	for _, se := range reply.Re {
+		name, ok := se.Map["name"]
+		if !ok {
+			continue
+		}
+
+		c.collectMetricsForInterface(name, se, ctx)
+	}
+
+	return nil
+}
+
+func (c *opticsCollector) collectMetricsForInterface(name string, se *proto.Sentence, ctx *collectorContext) {
+	for _, prop := range c.props {
+		v, ok := se.Map[prop]
+		if !ok {
+			continue
+		}
+
+		value, err := c.valueForKey(prop, v)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"device":    ctx.device.Name,
+				"interface": name,
+				"property":  prop,
+				"error":     err,
+			}).Error("error parsing interface monitor metric")
+			return
+		}
+
+		ctx.ch <- prometheus.MustNewConstMetric(c.descForKey(prop), prometheus.GaugeValue, value, ctx.device.Name, ctx.device.Address, name)
+	}
+}
+
+func (c *opticsCollector) valueForKey(name, value string) (float64, error) {
+	if name == "sfp-rx-loss" || name == "sfp-tx-fault" {
+		status := float64(1)
+		if value == "true" {
+			status = float64(0)
+		}
+
+		return status, nil
+	}
+
+	return strconv.ParseFloat(value, 64)
+}
+
+func (c *opticsCollector) descForKey(name string) *prometheus.Desc {
+	switch name {
+	case "sfp-rx-loss":
+		return c.rxStatusDesc
+	case "sfp-tx-fault":
+		return c.txStatusDesc
+	case "sfp-temperature":
+		return c.temperatureDesc
+	case "sfp-supply-voltage":
+		return c.voltageDesc
+	case "sfp-tx-bias-current":
+		return c.txBiasDesc
+	case "sfp-tx-power":
+		return c.txPowerDesc
+	case "sfp-rx-power":
+		return c.rxPowerDesc
+	}
+
 	return nil
 }
