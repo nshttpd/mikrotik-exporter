d33cbd193e5c529723053edee770dc92f2c03842
Nacef LABIDI <nacef.labidi@gmail.com>
2018-12-07 17:43:18 +0100

    Incorrect param for passing config file (#24)

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,5 +4,5 @@ if [ -z "$CONFIG_FILE" ]
 then
     /app/mikrotik-exporter -device $DEVICE -address $ADDRESS -user $USER -password $PASSWORD
 else
-    /app/mikrotik-exporter -config $CONFIG_FILE
+    /app/mikrotik-exporter -config-file $CONFIG_FILE
 fi
